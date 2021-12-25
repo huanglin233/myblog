19a4441cb020900248b008b67e23acc52f4087ed
huanglin <huanglin233>
2021-12-25 17:52:51 +0800

    增加网站底部备案信息

--- myblog_vue/src/components/FooterComponent.vue
+++ myblog_vue/src/components/FooterComponent.vue
@@ -3,6 +3,7 @@
         <footer class="ui vertical segment m-padded-tb-massive">
             <div class="ui center aligned container">
                 <p id="copyright" class="m-text-thin m-text-spaced m-opacity-tiny"></p>
+                <a class="m-text-thin m-text-spaced m-opacity-tiny" href="https://beian.miit.gov.cn/">蜀ICP备2020033033号-1</a>
             </div>
         </footer>
     </div>
